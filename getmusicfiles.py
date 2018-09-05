60d0260faa6ec539fa1748a77481edd5d9cc3fcf
shubham-rateria982 <shubham-rateria982@users.noreply.github.com>
2018-09-05 19:36:58 +0530

    Add files via upload

---
+++ getmusicfiles.py
@@ -0,0 +1,19 @@
+import os
+import fnmatch
+
+music_formats=['mp3','opus']
+path=['/home/shubham/Muzik','/mnt/0A1A6B571A6B3F37/soundcloudMusic']
+music_files=[]
+musicfiles=open('musicfiles','wb')
+def locate(pattern, root):
+    '''Locate all files matching supplied filename pattern in and below
+    supplied root directory.'''
+    for path, dirs, files in os.walk(os.path.abspath(root)):
+        for filename in fnmatch.filter(files, pattern):
+        	m=os.path.join(path, filename)
+        	print m 	
+        	musicfiles.write(m+'\n')
+def generatefilepaths():
+	for p in path:
+		locate('*.mp3',p)
+	musicfiles.close()
